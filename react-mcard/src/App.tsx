2b5305675982ee0ee941863f611ceb9f4ca54818
김민주 <ducco705@snu.ac.kr>
2024-04-18 15:37:32 +0900

    공통 컴포넌트 구현

--- react-mcard/src/App.tsx
+++ react-mcard/src/App.tsx
@@ -1,24 +1,72 @@
-import logo from './logo.svg'
 import './App.css'
+import Text from '@shared/Text'
+import Button from '@shared/Button'
+import Input from '@shared/Input'
+import TextField from '@shared/TextField'
+import Alert from '@shared/Alert'
+
+import { useAlertContext } from '@contexts/AlertContext'
 
 function App() {
+  const { open } = useAlertContext()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Text typography="t1" display="block" color="red">
+          t1
+        </Text>
+        <Text typography="t2" color="blue">
+          t2
+        </Text>
+        <Text typography="t3">t3</Text>
+        <Text typography="t4">t4</Text>
+        <Text>t5</Text>
+      </div>
+      <div>
+        <Button>클릭해주세요</Button>
+        <Button color="success">클릭해주세요</Button>
+        <Button color="error">클릭해주세요</Button>
+        <Button color="success" weak={true}>
+          클릭해주세요
+        </Button>
+        <Button color="success" weak={false}>
+          클릭해주세요
+        </Button>
+        <Button full={true}>클릭해주세요</Button>
+        <Button full={true} disabled={true}>
+          클릭해주세요
+        </Button>
+      </div>
+      <div style={{ height: 10, width: '100%' }}></div>
+
+      <Input placeholder="로그인" aria-invalid={false} />
+      <Input aria-invalid={true} />
+
+      <TextField label="아이디" />
+      <TextField label="패스워드" hasError={true} />
+
+      {/* <Alert
+        open={true}
+        title="Alert이 떴습니다."
+        onButtonClick={() => {}}
+        description="안녕하세요"
+      /> */}
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드 신청 완료',
+            description: '내역페이지에서 확인해주세요',
+            onButtonClick: () => {
+              console.log('Alert 버튼 클릭했습니다.')
+            },
+          })
+        }}
+      >
+        Alert 오픈
+      </Button>
+    </>
   )
 }
 
